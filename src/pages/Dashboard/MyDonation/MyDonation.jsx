20343089b4c3dce22a495fdcbb29cdd663302e4e
khadija-urmi <khadija5jahan211@gmail.com>
2025-01-26 07:52:22 +0600

    show data in all my doantionList

--- src/pages/Dashboard/MyDonation/MyDonation.jsx
+++ src/pages/Dashboard/MyDonation/MyDonation.jsx
@@ -5,38 +5,54 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const MyDonation = () => {
     const { currentUser } = useAuth();
-    //const axiosSecure = useAxiosSecure();
+    const axiosSecure = useAxiosSecure();
 
     const { data: donations = [] } = useQuery({
         queryKey: ['donations', currentUser?.email],
         queryFn: async () => {
-            // const res = await axiosSecure.get(`/save-donation-pay/${currentUser.email}`)
-            //return res.data;
+            const res = await axiosSecure.get(`/my-donation/${currentUser.email}`)
+            return res.data;
         }
     })
     console.log(donations)
     return (
         <div>
-            <h2 className="text3-xl">Total Payments: {donations.length}</h2>
-            <div className="overflow-x-auto">
-                <table className="table table-zebra">
-                    {/* head */}
-                    <thead>
+            <h2 className="text-3xl text-center py-4 mb-8 bg-gray-200">Total Payments: {donations.length}</h2>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
                         <tr>
-                            <th>Serial no</th>
-                            <th>Price</th>
-                            <th>Transaction Id</th>
-                            <th>Donation Campaign</th>
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                Serial no
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Price
+                            </th>
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                Transaction Id
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Donation Campaign
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        {donations.map((donation, index) => <tr key={donation._id}>
-                            <th>{index + 1}</th>
-                            <td>{donation?.donationAmount}</td>
-                            <td>{donation?.transactionId}</td>
-                            <td>{donation?._id}</td>
-                        </tr>)}
-
+                        {donations.map((donation, index) =>
+                            <tr key={donation._id} className="border-b border-gray-200 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                    {index + 1}
+                                </th>
+                                <td className="px-6 py-4">
+                                    {donation?.donationAmount}
+                                </td>
+                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                    {donation?.transactionId}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {donation?.donationEventName}
+                                </td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
