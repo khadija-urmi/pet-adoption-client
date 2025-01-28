434eca4d3ce7e1c496592babc70482c9a77d8637
khadija-urmi <khadija5jahan211@gmail.com>
2025-01-28 23:24:57 +0600

    change row  in allUser file

--- src/pages/Dashboard/AllUser/AllUser.jsx
+++ src/pages/Dashboard/AllUser/AllUser.jsx
@@ -49,17 +49,15 @@ const AllUser = () => {
                         {
                             userInfo.map((user) => (
                                 <tr key={user.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
 
-                                    </th>
                                     <td className="px-6 py-4">
-                                        {user.name}
+                                        {user?.name}
                                     </td>
                                     <td className="px-6 py-4">
-                                        {user.email}
+                                        {user?.email}
                                     </td>
                                     <td className="px-6 py-4">
-                                        {user.role === "admin" ? <button
+                                        {user?.role === "admin" ? <button
                                             onClick={() => handleMakeAdmin(user)}
                                             className="btn btn-lg p-2 bg-purple-500 rounded-md flex justify-center items-center">
                                             <RiAdminFill className="text-white 
